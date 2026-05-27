c2abdae195bc9f6a9041d89bfcfb13ea57e72cff
mouvnis <mouvnis@gmail.com>
2026-05-27 14:18:24 +0200

    Update MainActivity.java

--- android-patch/MainActivity.java
+++ android-patch/MainActivity.java
@@ -2,12 +2,10 @@ package com.nasstec.ocr;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.capacitorjs.plugins.admob.AdMobPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(AdMobPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
